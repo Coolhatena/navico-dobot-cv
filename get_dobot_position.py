60c82a1cf6427b69c4ac3851dd802105d4d3377b
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-02-23 15:15:16 -0800

    Add program to retrieve dobot current position

---
+++ get_dobot_position.py
@@ -0,0 +1,14 @@
+from send_command_with_response import send_command_with_response
+
+def get_dobot_position():
+	response = send_command_with_response(f"GetPose()")
+	start = response.index("{") + 1
+	end = response.index("}")
+	splitted_data = response[start:end].split(',')
+	position = [float(splitted_data[0]), float(splitted_data[1]), float(splitted_data[2]), float(splitted_data[3])]
+
+	return position
+
+
+if __name__ == "__main__":
+	print(get_dobot_position())

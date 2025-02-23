0c7dd126b8928160e378d88dfad87181bc2f07dd
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-02-22 16:36:06 -0800

    Make send_command a standalone file

---
+++ send_command.py
@@ -0,0 +1,20 @@
+import socket
+
+# Function to send a command to the robot
+def send_command(command, port=30003):
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect(('192.168.1.6', port))
+		s.sendall(command.encode())
+		response = s.recv(1024)
+		print('Received:', response.decode())
+		
+
+if __name__ == "__main__":
+	import time
+	
+	send_command("ClearError()", port=29999)
+	send_command("EnableRobot()", port=29999)
+	 
+	time.sleep(5)
+	
+	send_command("DisableRobot()", port=29999)

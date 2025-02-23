9e2832763d83a6f8b4fb39437e1681b72ddad2c4
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-02-23 14:18:39 -0800

    Make rotate_tool a portable function

--- rotate_tool.py
+++ rotate_tool.py
@@ -1,16 +1,19 @@
 from send_command import send_command
 from send_command_with_response import send_command_with_response
 
+def rotate_tool():
+	speed_slow = 1
+	acc_slow = 1
 
-speed_slow = 1
-acc_slow = 1
+	response = send_command_with_response(f"GetPose()")
+	start = response.index("{") + 1
+	end = response.index("}")
+	positions = response[start:end].split(',')
 
-response = send_command_with_response(f"GetPose()")
-start = response.index("{") + 1
-end = response.index("}")
-positions = response[start:end].split(',')
+	tool_angle = float(positions[3])
 
-tool_angle = float(positions[3])
+	rotation = -180 if tool_angle > -200 else 180
+	send_command(f"RelMovJUser(0, 0, 0, {rotation}, SpeedJ={speed_slow},AccJ={acc_slow})")
 
-rotation = -180 if tool_angle > -200 else 180
-send_command(f"RelMovJUser(0, 0, 0, {rotation}, SpeedJ={speed_slow},AccJ={acc_slow})")
+if __name__ == "__main__":
+	rotate_tool()

2f3aacafc6665e181a52120bb96b5c6eeec7fab8
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-02 17:13:16 -0800

    Remove redundant comments

--- disable_dobot.py
+++ disable_dobot.py
@@ -2,7 +2,5 @@ import socket
 
 from tcp_dobot import send_command
 
-# Clear errors and enable the robot
 send_command("ClearError()", port=29999)
-# Send command to disable robot
 send_command("DisableRobot()", port=29999)

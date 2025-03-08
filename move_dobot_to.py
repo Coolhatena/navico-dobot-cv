00f0f1bc21686b57d4b63f445dde15d3db5765f3
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-07 16:58:11 -0800

    Use custom function to compare the difference between points

--- move_dobot_to.py
+++ move_dobot_to.py
@@ -2,14 +2,19 @@ from tcp_dobot import send_command
 from get_dobot_position import get_dobot_position
 
 
+# Position need to be compared this way because there may be a slight difference 
+# in the position values when the dobot is moving
+def _compare_positions(position1, position2, max_difference=0.5):
+	return all(abs(x - y) < max_difference for x, y in zip(position1, position2))
+
 def moveDobotTo(point, speed=1, acc=1, port=30003):
 	x, y, z, r = point
 	command = "MovJ({}, {}, {}, {}, SpeedJ={}, AccJ={})".format(x, y, z, r, speed, acc)
 	send_command(command, port)
 
-	is_position_reached = point == get_dobot_position()
+	is_position_reached = _compare_positions(point, get_dobot_position())
 	while not is_position_reached:
-		is_position_reached = point == get_dobot_position()
+		is_position_reached = _compare_positions(point, get_dobot_position())
 
 
 def moveDobotToRelative(point, speed=1, acc=1, port=30003):
@@ -19,10 +24,15 @@ def moveDobotToRelative(point, speed=1, acc=1, port=30003):
 
 	command = "RelMovJUser({}, {}, {}, {}, SpeedJ={}, AccJ={})".format(x, y, z, r, speed, acc)
 	send_command(command, port)
-
-	is_position_reached = new_position == get_dobot_position()
+	
+	
+	is_position_reached = _compare_positions(new_position, get_dobot_position()) 
 	while not is_position_reached:
-		is_position_reached = new_position == get_dobot_position()
+		is_position_reached = _compare_positions(new_position, get_dobot_position())
+		print(f"Current position: {get_dobot_position()}")
+		print(f"Should be: {new_position}")
+
+	print(f'MOVEMENT TO {point} DONE\n\n') 
 
 if __name__ == "__main__":
 	send_command("ClearError()", port=29999)

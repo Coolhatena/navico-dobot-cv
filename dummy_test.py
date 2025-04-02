25f1ec7e66f1eafdfe6f74b9eb8d84ed89f59f52
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-02 16:43:47 -0700

    Add more refined dummy test

--- dummy_test.py
+++ dummy_test.py
@@ -11,5 +11,27 @@ def dummy_test():
 	moveDobotToRelative((0, 0, test_movement, 0), speed_slow, acc_slow)
 	
 
+def full_dummy_test():
+	# Define the options
+	speed_slow = 2
+	acc_slow = 2
+
+	down_movement = 3
+	side_movement = 30
+
+	# Move torquemeter down
+	moveDobotToRelative((0, 0, -down_movement, 0), speed_slow, acc_slow)
+
+	# # Move torquimeter to the sides
+	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, -side_movement, 0, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, -side_movement, 0, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
+
+
+	# # Move torquemeter up
+	moveDobotToRelative((0, 0, down_movement, 0), speed_slow, acc_slow)
+
+
 if __name__ == "__main__":
-	dummy_test()
+	full_dummy_test()

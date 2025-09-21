883e8853fdd1ea1f4ab5ee330e3cfce283be8c7d
Coolhatena <ingsc.geffroy@gmail.com>
2025-09-21 04:47:49 -0700

    Add dummy test with params

--- dummy_test.py
+++ dummy_test.py
@@ -13,19 +13,40 @@ def dummy_test():
 
 def full_dummy_test():
 	# Define the options
-	speed_slow = 2
-	acc_slow = 2
+	speed_slow = 1
+	acc_slow = 1
 
-	down_movement = 3
-	side_movement = 30
+	down_movement = 12
+	side_movement = 35
 
 	# Move torquemeter down
 	moveDobotToRelative((0, 0, -down_movement, 0), speed_slow, acc_slow)
 
 	# # Move torquimeter to the sides
 	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
-	moveDobotToRelative((0, -side_movement, 0, 0), speed_slow, acc_slow)
-	moveDobotToRelative((0, -side_movement, 0, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, -side_movement, 1, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, -side_movement, -1, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
+
+
+	# # Move torquemeter up
+	moveDobotToRelative((0, 0, down_movement, 0), speed_slow, acc_slow)
+
+
+def modular_full_dummy_test(down_movement, side_movement):
+	# Define the options
+	speed_slow = 1
+	acc_slow = 1
+
+	# Move torquemeter down
+	moveDobotToRelative((0, 0, -down_movement, 0), speed_slow, acc_slow)
+
+	# # Move torquimeter to the sides
+	# Right
+	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
+	moveDobotToRelative((0, -side_movement, 1, 0), speed_slow, acc_slow)
+	# Left
+	moveDobotToRelative((0, -side_movement, -1, 0), speed_slow, acc_slow)
 	moveDobotToRelative((0, side_movement, 0, 0), speed_slow, acc_slow)
 
 

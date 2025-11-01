fb34ee6ed1100232f948efce2fb0a35cbeac1fb3
Coolhatena <ingsc.geffroy@gmail.com>
2025-11-01 16:52:18 -0700

    Create relay control helper

--- relay_control.py
+++ relay_control.py
@@ -1,15 +1,16 @@
-import serial, time
+import serial
 
-PORT = "/dev/ttyUSB0"
-BAUD = 9600
 
-cmds = {
-    "ON_hex" : bytes([0xA0,0x01,0x01,0xA2]),
-    "OFF_hex": bytes([0xA0,0x01,0x00,0xA1]),
-}
+def switchRelay(turnOn = True):
+	PORT = "/dev/ttyUSB0"
+	BAUD = 9600
 
-with serial.Serial(PORT, BAUD, timeout=0.2) as s:
-    s.write(cmds["ON_hex"])  
-    time.sleep(1)
-    s.write(cmds["OFF_hex"]) 
-    time.sleep(1)
+	cmds = {
+		"ON_hex" : bytes([0xA0,0x01,0x01,0xA2]),
+		"OFF_hex": bytes([0xA0,0x01,0x00,0xA1]),
+	}
+
+	state = "ON_hex" if turnOn else "OFF_hex"
+
+	with serial.Serial(PORT, BAUD, timeout=0.2) as s:
+		s.write(cmds[state])
